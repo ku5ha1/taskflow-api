beddbfc9a117e54213f6e36bc6d5c8a55c7daffc
kushalraga <kushalraga20@gmail.com>
2025-09-12 12:58:35 +0530

    call init_db on startup

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI 
+from app.utils.database import Base, engine
 
 app = FastAPI() 
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 async def get_health():
     return {"message": "API running successfully"}
+
+@app.on_event("startup")
+async def startup_event():
+    init_db()
