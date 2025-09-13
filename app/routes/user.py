3fb14daa077e840583b5ae2f389aa066d0e9e3a7
kushalraga <kushalraga20@gmail.com>
2025-09-13 23:19:44 +0530

    eliminate user registration

--- app/routes/user.py
+++ app/routes/user.py
@@ -4,30 +4,64 @@ from app.schemas.user import UserCreate, UserOut, UserLogin
 from app.utils.auth import hash_password, create_access_token, get_current_user, verify_password
 from app.utils.database import get_db
 from sqlalchemy.orm import Session
+from app.utils.depedency import admin_required
 
 router = APIRouter(prefix="/user", tags=["User"])
 
-@router.post("/register", response_model=UserOut)
-async def user_register(user: UserCreate, db: Session = Depends(get_db)):
+# @router.post("/register", response_model=UserOut)
+# async def user_register(user: UserCreate, db: Session = Depends(get_db)):
+#     db_user = db.query(User).filter(
+#         User.username == user.username or User.email == user.email
+#     ).first()
+#     if db_user:
+#         raise HTTPException(
+#             status_code=400,
+#             detail="Username or Email already in use"
+#         )    
+#     hashed_password = hash_password(user.password)
+#     new_user = User(
+#         username = user.username,
+#         email = user.email,
+#         hashed_password = hashed_password
+#     )
+#     db.add(new_user)
+#     db.commit()
+#     db.refresh(new_user)
+    
+#     return new_user
+
+@router.post("/create", response_model=UserOut)
+async def create_user(
+    user_data: UserCreate, 
+    current_user: User = Depends(admin_required),
+    db: Session = Depends(get_db)
+):
     db_user = db.query(User).filter(
-        User.username == user.username or User.email == user.email
+        User.email == user_data.email
     ).first()
     if db_user:
         raise HTTPException(
-            status_code=400,
-            detail="Username or Email already in use"
-        )    
-    hashed_password = hash_password(user.password)
-    new_user = User(
-        username = user.username,
-        email = user.email,
-        hashed_password = hashed_password
-    )
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    
-    return new_user
+            status_code=409,
+            detail=f"Email already registered"
+        )
+    hashed_password = hash_password(user_data.password)
+    try:
+        new_user = User(
+            username = user_data.username,
+            email = user_data.email,
+            hashed_password = hashed_password
+        )
+        db.add(new_user)
+        db.commit()
+        db.refresh(new_user)
+        return new_user
+    except Exception as e:
+        db.rollback()
+        print(f"Exception: {e}")
+        raise HTTPException(
+            status_code=500,
+            detail="Could not add user"
+        )
 
 @router.post("/login")
 async def user_login(user: UserLogin, db: Session = Depends(get_db)):
