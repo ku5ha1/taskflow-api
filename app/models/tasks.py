c66e594aa7dfb1a7af40018da8d55fd557aed22e
kushalraga <kushalraga20@gmail.com>
2025-09-23 17:39:56 +0530

    tasks models + schemas

---
+++ app/models/tasks.py
@@ -0,0 +1,19 @@
+from app.utils.database import Base 
+from sqlalchemy import Column, ForeignKey, String, Integer, DateTime
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship 
+
+class Task(Base):
+    __tablename__ = "tasks"
+    
+    id = Column(Integer, primary_key=True, unique=True)
+    name = Column(String(50))
+    description = Column(String(50))
+    status = Column(String(20))
+    priority = Column(String(10))
+    due_date = Column(DateTime)
+    project_id = Column(Integer, ForeignKey('projects.id'), nullable=False)
+    assigned_to_user = Column(Integer, ForeignKey('project_members.id'))
+    
+    project = relationship('Project', back_populates='tasks')
+    assigned_to = relationship('ProjectMembers', back_populates='tasks')
---
+++ app/schemas/tasks.py
@@ -0,0 +1,43 @@
+from pydantic import BaseModel, Field
+import enum 
+from typing import Optional, List
+from datetime import datetime
+
+class TaskPriority(str, enum.Enum):
+    HIGH = "high"
+    MEDIUM = "medium"
+    LOW = "low"
+    
+class TaskStatus(str, enum.Enum):
+    PENDING = "pending"
+    IN_PROGRESS = "in-progress"
+    COMPLETED = "completed"
+    
+class TaskCreate(BaseModel):
+    name: str 
+    description: str 
+    status: TaskStatus = Field(default=TaskStatus.PENDING)
+    priority: TaskPriority 
+    due_date: datetime 
+    project_id: int 
+    assigned_to: int
+
+class TaskOut(BaseModel):
+    id: int 
+    name: str 
+    description: str
+    status: TaskStatus
+    priority: TaskPriority 
+    due_date: datetime 
+    project_id: int 
+    assigned_to: int
+    
+class TaskUpdate(BaseModel):
+    name: Optional[str] = None 
+    description: Optional[str] = None
+    status: Optional[TaskStatus] = None 
+    priority: Optional[TaskPriority] = None 
+    due_date: Optional[datetime] = None
+    
+class TaskList(BaseModel):
+    tasks: List[TaskOut]
